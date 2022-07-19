c777eb768e0e7e7cb455bd101049bf6a52950927
martin.giacon <martin.giacon@fresenius-kabi.com>
2022-07-19 17:39:59 -0300

    Entrega 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
+    
     <div className="App">
+<NavBar/>
       <header className="App-header">
        <h1>Retro Game Store</h1>
+       
        <a href='https://github.com/elalearg/RetroGameStore'> Github</a>
       </header>
     </div>
